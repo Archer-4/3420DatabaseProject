c54b61c9106dc4a4533d20f98ffc1711347be472
Archer-4 <njackson6@csub.edu>
2020-12-10 21:44:13 -0800

    Added clients_equipment View

--- CreateViews.sql
+++ CreateViews.sql
@@ -8,3 +8,9 @@ SELECT client.name AS ClientName, Employees.name AS EmployeeName, employees.empl
 FROM employees, client
 WHERE client.clientID = employees.clientID
 ORDER BY pay DESC;
+
+CREATE VIEW Clients_Equipment AS
+SELECT c.name AS Name, e.equipmenttype AS equipment, e.brand AS brand, e.status AS current_status
+FROM client c, equipment e, field_info f
+WHERE c.clientID = f.clientID
+AND f.fieldID = e.fieldID;
