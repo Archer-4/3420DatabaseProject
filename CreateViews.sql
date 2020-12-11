0839483892d21e9f56eac6950e34f2318040fd88
Archer-4 <njackson6@csub.edu>
2020-12-10 21:27:27 -0800

    Added Employee_Roster View

--- CreateViews.sql
+++ CreateViews.sql
@@ -2,3 +2,8 @@ CREATE VIEW Inventory_Log AS
 SELECT client.name AS clientName, inventory.Name AS stockName, inventory.Amount
 FROM inventory, client
 WHERE client.clientID = inventory.clientID;
+
+CREATE VIEW Employee_Roster AS
+SELECT client.name AS ClientName, Employees.name AS EmployeeName, employees.employmentstatus AS job, employees.salary AS pay
+FROM employees, client
+WHERE client.clientID = employees.clientID;
