044aef31aa22b796d060674290a2b044ea447e5f
Archer-4 <njackson6@csub.edu>
2020-12-10 21:35:46 -0800

    Tweaked Employee_Roster

--- CreateViews.sql
+++ CreateViews.sql
@@ -6,4 +6,5 @@ WHERE client.clientID = inventory.clientID;
 CREATE VIEW Employee_Roster AS
 SELECT client.name AS ClientName, Employees.name AS EmployeeName, employees.employmentstatus AS job, employees.salary AS pay
 FROM employees, client
-WHERE client.clientID = employees.clientID;
+WHERE client.clientID = employees.clientID
+ORDER BY pay DESC;
