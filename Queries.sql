1f8ecef3aa157df0f100c14f5b9e9f78f78b24f0
Archer-4 <njackson6@csub.edu>
2020-12-11 01:01:02 -0800

    Re-added old simple queries.

--- Queries.sql
+++ Queries.sql
@@ -7,3 +7,41 @@ INNER JOIN employees ON employees.clientID = client.clientID
 WHERE CropType = 'Nut'
 GROUP BY client.clientID
 HAVING Count(employeeID) > 2;
+--Select employees who have a salary of over 15k
+SELECT name
+FROM employees
+WHERE (Salary > 15000);
+
+--Select Fields that grow vegetables
+SELECT fieldID
+FROM field_info f, crops c
+WHERE f.cropID = c.cropID
+AND CropType = 'Vegetable';
+
+--Select employees who work for client 2
+SELECT name
+FROM employees
+WHERE clientID = 2;
+
+--Select equipment that is not in good status
+SELECT *
+FROM Equipment
+WHERE status = 'good';
+
+--Select clients with at least 2 outgoing deliveries
+SELECT *
+FROM (SELECT name, COUNT(client.clientID) deliveries
+      FROM client
+      JOIN outgoingdeliveries ON outgoingdeliveries.clientID = client.clientID
+      GROUP BY name) t
+WHERE deliveries > 1;
+
+--Select clients with at least 2 incoming deliveries
+SELECT *
+FROM (SELECT name, COUNT(client.clientID) deliveries
+      FROM client
+      JOIN incomingdeliveries ON incomingdeliveries.clientID = client.clientID
+      GROUP BY name) t
+WHERE deliveries > 1;
+
+--
